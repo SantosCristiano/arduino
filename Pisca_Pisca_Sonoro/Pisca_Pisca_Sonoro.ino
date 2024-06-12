5e5ac737d14ea7205e4685501de5998a8e297b21
Christiano Sanctorum <santosdesouzacris@gmail.com>
2024-06-12 01:39:16 -0300

    Up files

---
+++ Pisca_Pisca_Sonoro/Pisca_Pisca_Sonoro.ino
@@ -0,0 +1,17 @@
+/*******************************************************************************
+* RoboCore Kit Iniciante V8 para Arduino - Fazendo Barulho 1
+* Pisca-pisca sonoro.
+*******************************************************************************/
+
+int frequencia = 100;
+
+void setup(){
+  pinMode(10, OUTPUT); // configura o pino com o buzzer como saída
+}
+
+void loop(){
+  tone(10, frequencia); // gera frequencia de 2000Hz no buzzer
+  delay(1000);
+  noTone(10); // para frequencia no buzzer
+  delay(1000);
+}
